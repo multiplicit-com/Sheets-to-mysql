50852b6ecd7ede00a4cf990c7199ca424492a1d3
Multiplicit.co.uk <127529943+multiplicit-com@users.noreply.github.com>
2023-09-28 11:44:35 +0100

    Update import.php
    Simplified code

--- import.php
+++ import.php
@@ -2,98 +2,122 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-// Configurable Variables
-$googleAccountKeyFilePath = __DIR__ . '/credentials.json';
-
-$dbHost = 'localhost';
-$dbUsername = 'username';
-$dbPassword = 'password';
-$dbName = 'database_name';
-
-$spreadsheetId = 'your-spreadsheet-id';
-$createTables = false;
-
-// Configuration array for sheets
-$config = [
-    'Sheet1' => ['mode' => 'replace'],
-    'Sheet2' => ['mode' => 'append'],
-    'Sheet3' => ['mode' => 'unique', 'unique_column' => 'id'], // 'id' should be the name of the column you are checking for uniqueness
-    // Add more sheets as needed
+// User Configurable Settings
+$dbConfig = [
+    'host' => 'localhost',
+    'username' => 'username',
+    'password' => 'password',
+    'dbname' => 'database_name',
 ];
 
-class GoogleSheetToMySQL
-{
+$sheetConfig = [
+    'Sheet1' => ['mode' => 'replace'],
+    'Sheet2' => ['mode' => 'append'],
+    'Sheet3' => ['mode' => 'unique', 'unique_column' => 'id'],
+];
+
+$spreadsheetId = 'your-spreadsheet-id';
+$createTables = true;
+
+class GoogleSheetImporter {
     private $service;
     private $conn;
-    private $spreadsheetId;
+    private $config;
     private $createTables;
+    private $spreadsheetId;
     
-    public function __construct($service, $conn, $spreadsheetId, $createTables)
-    {
-        $this->service = $service;
-        $this->conn = $conn;
+    public function __construct($spreadsheetId, $dbConfig, $config, $createTables) {
+        $client = new Google_Client();
+        $client->setApplicationName('Google Sheets API PHP Quickstart');
+        $client->setScopes(Google_Service_Sheets::SPREADSHEETS_READONLY);
+        $client->setAuthConfig(__DIR__ . '/credentials.json');
+        
+        $this->service = new Google_Service_Sheets($client);
+        $this->conn = new mysqli($dbConfig['host'], $dbConfig['username'], $dbConfig['password'], $dbConfig['dbname']);
+        $this->config = $config;
         $this->spreadsheetId = $spreadsheetId;
         $this->createTables = $createTables;
+        
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
     }
     
-    public function importData($config)
-    {
-        foreach ($config as $sheetName => $options) {
-            $mode = $options['mode'];
-            
-            // Fetch data from Google Sheet
-            $response = $this->service->spreadsheets_values->get($this->spreadsheetId, $sheetName);
+    public function importSheets() {
+        foreach ($this->config as $sheetTitle => $sheetConfig) {
+            $range = $sheetTitle;
+            $response = $this->service->spreadsheets_values->get($this->spreadsheetId, $range);
             $values = $response->getValues();
             
-            if (empty($values)) {
-                print "No data found.\n";
+            if (!$values) {
+                echo "No data found for $sheetTitle.\n";
                 continue;
             }
 
-            // Extract header and rows from fetched data
-            $header = $values[0];
-            $rows = array_slice($values, 1);
+            $header = array_shift($values);
+            $tableName = preg_replace('/\W+/', '', strtolower($sheetTitle));
+
+            if ($this->createTables) {
+                $this->createOrUpdateTable($tableName, $header);
+            }
             
-            // Process data according to the mode
-            if ($mode === 'replace') {
-                $this->replaceData($sheetName, $header, $rows);
-            } elseif ($mode === 'append') {
-                $this->appendData($sheetName, $header, $rows);
-            } else {
-                // Handle other modes as needed
+            switch ($sheetConfig['mode']) {
+                case 'replace':
+                    $this->conn->query("DELETE FROM `$tableName`");
+                    // Fallthrough intended
+                case 'append':
+                    $this->insertRows($tableName, $header, $values);
+                    break;
+                case 'unique':
+                    $uniqueColumn = $sheetConfig['unique_column'];
+                    $this->insertUniqueRows($tableName, $header, $values, $uniqueColumn);
+                    break;
+            }
+        }
+        
+        $this->conn->close();
+    }
+    
+    private function createOrUpdateTable($tableName, $header) {
+        $columns = [];
+        foreach ($header as $field) {
+            $fieldName = preg_replace('/\W+/', '', strtolower($field));
+            $columns[] = "`$fieldName` TEXT";
+        }
+
+        $columnsSQL = implode(", ", $columns);
+        $sql = "CREATE TABLE IF NOT EXISTS `$tableName` ($columnsSQL)";
+        if ($this->conn->query($sql) === FALSE) {
+            echo "Error creating table $tableName: " . $this->conn->error;
+        }
+    }
+
+    private function insertRows($tableName, $header, $rows) {
+        foreach ($rows as $row) {
+            $values = array_map([$this->conn, 'real_escape_string'], $row);
+            $valuesSQL = implode("', '", $values);
+            $sql = "INSERT INTO `$tableName` (`" . implode('`, `', $header) . "`) VALUES ('$valuesSQL')";
+            if ($this->conn->query($sql) === FALSE) {
+                echo "Error inserting row into $tableName: " . $this->conn->error;
             }
         }
     }
 
-    private function replaceData($tableName, $header, $rows)
-    {
-        // Implementation for replacing data in the table
+    private function insertUniqueRows($tableName, $header, $rows, $uniqueColumn) {
+        foreach ($rows as $row) {
+            $values = array_map([$this->conn, 'real_escape_string'], $row);
+            $valuesSQL = implode("', '", $values);
+            $uniqueValue = $this->conn->real_escape_string($row[array_search($uniqueColumn, $header)]);
+            $sql = "INSERT INTO `$tableName` (`" . implode('`, `', $header) . "`) VALUES ('$valuesSQL') 
+                    ON DUPLICATE KEY UPDATE `$uniqueColumn` = '$uniqueValue'";
+            if ($this->conn->query($sql) === FALSE) {
+                echo "Error inserting unique row into $tableName: " . $this->conn->error;
+            }
+        }
     }
-
-    private function appendData($tableName, $header, $rows)
-    {
-        // Implementation for appending data to the table
-    }
-
-    // ... additional methods as necessary
 }
 
-// Google Sheets API setup
-$client = new Google_Client();
-$client->setApplicationName('Google Sheets API PHP Quickstart');
-$client->setScopes(Google_Service_Sheets::SPREADSHEETS_READONLY);
-$client->setAuthConfig($googleAccountKeyFilePath);
+$importer = new GoogleSheetImporter($spreadsheetId, $dbConfig, $sheetConfig, $createTables);
+$importer->importSheets();
 
-$service = new Google_Service_Sheets($client);
-
-// MySQL database setup
-$conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$importer = new GoogleSheetToMySQL($service, $conn, $spreadsheetId, $createTables);
-$importer->importData($config);
-
-$conn->close();
+?>
